a0771a187786e23b5ccf37f8d95d403af60a9fe5
Uxue Mancisidor laskurain <uxmancis@c4r6s6.42urduliz.com>
2025-10-28 11:00:17 +0100

    ex00 wip

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: uxmancis <uxmancis>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:52:25 by uxmancis          #+#    #+#             */
-/*   Updated: 2025/10/27 14:25:02 by uxmancis         ###   ########.fr       */
+/*   Updated: 2025/10/28 10:59:45 by uxmancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,85 @@
 #include <string> //std::stoi
 #include <cstdlib> //std::stoi
 
+/* isValidDate
+*
+*   Prints messages when date is invalid.
+*
+*   Returns:
+*       true -> Valid date
+*       false -> Invalid date
+*/
 bool isValidDate(std::string date)
 {
     /* Year */
-    int year = stoi(date.substr(0,4)); //substr(where to start, how many chars to get)
+    int year = atoi(date.substr(0,4).c_str()); //substr(where to start, how many chars to get)
     std::cout << "Year: " << year << std::endl;
 
     /* Month */
-    int month = stoi(date.substr(5,2)); //substr(where to start, how many chars to get)
+    int month = atoi(date.substr(5,2).c_str()); //substr(where to start, how many chars to get)
     std::cout << "month: " << month << std::endl;
 
     /* Day */
-    int day = stoi(date.substr(8,2));;
+    int day = atoi(date.substr(8,2).c_str());;
     std::cout << "day: " << day << std::endl;
 
-
-    if (year >= 0 || year <= 2100
+    /* Invalid date - Generic */
+    if (year < 0 || year > 2100
         || month < 1 || month > 12
         || date[4] != '-' || date[7] != '-'
         || day < 1 || day > 31)
     {
-        std::cerr << "Error: invalid month in date" << std::endl;
+        std::cerr << "Error: bad input" <<  " => " << date << std::endl;
         return(false);
     }
-
+    
+    /* Invalid date - Particular case: February not leap-year, 28th max*/
+    if (month == 2)
+    {
+        if (day > 28)
+        {
+            std::cerr << "Error: bad input" <<  " => " << date << std::endl;
+            return(false);
+        }
+    }
     return(true);
+}
+
+/* isValidValue
+*
+*   Prints messages when value is invalid.
+*
+*   Returns:
+*       true -> Valid value
+*       false -> Invalid value
+*/
+bool isValidValue(std::string v)
+{
+    std::cout << "----------------------------" << std::endl;
+    std::cout << "isValidValue, Value = " << v << std::endl;
+    std::cout << "size = " << v.size() << std::endl;
+    
+    if (v.size() > 10) /* Cuidado! 10 is max int */
+    {
+        std::cout << "Error: too large a number." << std::endl;
+        return (false);
+    }
+        
+
+    long int value = atoi(v.c_str());
+
+    
+
+    /* Negative values are not accepted */
+    if (value < 0)
+    {
+        std::cout << "Error: not a positive number." << std::endl;
+        return (false);
+    }
+        
+        
+    std::cout << "Valueeee :) = " << value << std::endl;
+    return (true);
 }
 
 void validLine(std::string line, std::map<std::string, float> result)
@@ -55,13 +109,14 @@ void validLine(std::string line, std::map<std::string, float> result)
     {
         std::string date = line.substr(0,10); //get date
         std::cout << "Date: " << date << std::endl;
-        isValidDate(date);
+        isValidDate(date); /* prints invalid line errors */
     }
 
     if (line.size() >= 12)
     {
-        std::string value = line.substr(12); //get date
+        std::string value = line.substr(13); //get date
         std::cout << "Value: " << value << std::endl;
+        isValidValue(value);
     }
 
     std::cout << "----------------------\n" << std::endl;
@@ -95,7 +150,8 @@ int main(int argc, char **argv)
 
     while(getline(file, line)) //Iteration in each line
     {
-        validLine(line, result);
+        if (line != "date | value") /* "date | value" 1st line is ignored. */
+            validLine(line, result);
             //print
         // std::string date = line.substr(0,10);
         // std::string value = line.substr(12);
